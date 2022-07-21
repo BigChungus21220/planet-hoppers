985fcd19db0952c4d8d9daa18a329f1911ef206b
BigChungus21220 <109574168+BigChungus21220@users.noreply.github.com>
2022-07-20 22:54:31 -1000

    Delete 8k_jam_b directory

--- 8k_jam_b/functions/spaceship_still.mcfunction
+++
@@ -1,3 +0,0 @@
-playanimation @p[r=2] animation.invis
-event entity @p ek:fill_oxygen
-event entity @s ek:decrement_fuel
--- 8k_jam_b/scripts/Main.js
+++
@@ -1,4 +0,0 @@
-import 'scripts/SpawnParticles.js';
-import 'scripts/SetWorldRules.js';
-import 'scripts/Oxygen.js';
-import 'scripts/Spaceship.js';
